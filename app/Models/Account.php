aafa9e0ffd874a73a5a3fbe69d73ee2352a98312
Tim Cortesi <tcortesi@gmail.com>
2021-10-05 19:28:12 -0400

    Change Class Include Case

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use App\Libraries\HttpHelper;
+use App\Libraries\HTTPHelper;
 use Illuminate\Support\Arr;
 
 class Account extends Model
