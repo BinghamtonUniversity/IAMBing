1640994457bc5ac930087eacf206ffe192535e9a
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2022-03-02 11:33:14 -0500

    fix trust proxies issue for HTTPS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,8 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ForceHTTPS::class,
-
+            \App\Http\Middleware\ForceHTTPS::class
         ],
         'api' => [
             'throttle:60,1',
