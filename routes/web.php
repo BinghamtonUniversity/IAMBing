914196894e9c5b264b87493fe5a7fe8de74d00c9
Tim Cortesi <tcortesi@gmail.com>
2022-07-13 11:08:48 -0400

    Fix permissions for systems

--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::group(['middleware'=>['custom.auth']], function () {
 
         /* Systems Methods */
         Route::get('/systems',[SystemController::class,'get_all_systems'])->middleware('can:list_search,App\Models\System');
-        Route::get('/systems/{system}',[SystemController::class,'get_system'])->middleware('can:manage_systems,App\Models\System');
+        Route::get('/systems/{system}',[SystemController::class,'get_system'])->middleware('can:list_search,App\Models\System');
         Route::post('/systems',[SystemController::class,'add_system'])->middleware('can:manage_systems,App\Models\System');
         Route::put('/systems/{system}',[SystemController::class,'update_system'])->middleware('can:manage_systems,App\Models\System');
         Route::delete('/systems/{system}',[SystemController::class,'delete_system'])->middleware('can:manage_systems,App\Models\System');
