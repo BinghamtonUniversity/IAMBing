56c22ac8d9c0c62f150f7c992c033ca54ac28aff
Tim Cortesi <tcortesi@gmail.com>
2022-08-31 07:16:11 -0400

    Get all Future Impact

--- app/Http/Controllers/IdentityController.php
+++ app/Http/Controllers/IdentityController.php
@@ -38,7 +38,7 @@ class IdentityController extends Controller
         $identity->primary_affiliation = isset($identity->affiliations[0])?$identity->affiliations[0]:null;
         $identity->sponsored_entitlements = IdentityEntitlement::where('type','add')->where('sponsor_id',$identity->id)->with('identity')->with('entitlement')->get();
         $identity->identity_entitlements_with_sponsors = IdentityEntitlement::where('type','add')->where('identity_id',$identity->id)->whereNotNull('sponsor_id')->with('sponsor')->with('entitlement')->get();
-        $identity->future_impact = $identity->future_impact_calculate(true);
+        $identity->future_impact = $identity->future_impact_calculate(false);
         return $identity;
     }
 
