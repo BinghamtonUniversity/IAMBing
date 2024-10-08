7bc9e904825d0eca7a0183e3a89c09b2a8c37acd
Tim Cortesi <tcortesi@gmail.com>
2024-10-08 09:20:35 -0400

    Remove CAS Auth Check before CAS Auth

--- app/Http/Controllers/CASController.php
+++ app/Http/Controllers/CASController.php
@@ -9,7 +9,7 @@ use App\Models\IdentityUniqueIDs;
 
 class CASController extends Controller {
     public function login(Request $request) {
-        if(!Auth::check() && !cas()->checkAuthentication()) {
+        if(!Auth::check()) {
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             }
